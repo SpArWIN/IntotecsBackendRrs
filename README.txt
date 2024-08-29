a2857d05674b6a53e3d0b69f20576e821cfd527d
TotKtoVseZnaet <nikola10www@mail.ru>
2024-08-29 20:32:24 +0300

    nginx

---
+++ README.txt
@@ -0,0 +1,1 @@
+https://github.com/SpArWIN/IntotecsBackendRrs
--- infoFront/src/Shared/API/api.tsx
+++ infoFront/src/Shared/API/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const localapi = axios.create({
-  baseURL: "/api",
+  baseURL: "/",
   headers: { "Content-Type": "application/json" },
 });
